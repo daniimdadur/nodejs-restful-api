13ae6f5fde0fdf383e501d0de0ac3aedc2455995
Dani Imdadur <dir131203@gmail.com>
2025-01-20 17:07:53 +0700

    feat: patch service test

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,8 +1,14 @@
 import {validate} from "../validation/validation.js";
-import {registerUserValidation} from "../validation/user-validation.js";
+import {
+    getUserValidation,
+    loginUserValidation,
+    registerUserValidation,
+    updateUserValidation
+} from "../validation/user-validation.js";
 import {prismaClient} from "../application/database.js";
 import {ResponseError} from "../error/response-error.js";
 import bcrypt from "bcrypt"
+import {v7 as uuid} from "uuid"
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request);
@@ -28,6 +34,98 @@ const register = async (request) => {
     });
 }
 
+const login = async (request) => {
+    const loginRequest = validate(loginUserValidation, request);
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: loginRequest.username,
+        },
+        select: {
+            username: true,
+            password: true
+        }
+    });
+
+    if (!user) {
+        throw new ResponseError(401, 'username or password wrong');
+    }
+
+    const isPasswordValid = await bcrypt.compare(loginRequest.password, user.password);
+    if (!isPasswordValid) {
+        throw new ResponseError(401, 'username or password wrong');
+    }
+
+    const token = uuid().toString();
+    return prismaClient.user.update({
+        data: {
+            token: token
+        },
+        where: {
+            username: user.username
+        },
+        select: {
+            token: true
+        }
+    });
+}
+
+const get = async (username) => {
+    username = validate(getUserValidation, username);
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        },
+        select: {
+            username: true,
+            name: true
+        }
+    });
+
+    if (!user) {
+        throw new ResponseError(404, 'user is not found');
+    }
+
+    return user;
+}
+
+const update = async (request) => {
+    const user = validate(updateUserValidation, request);
+
+    const totalUserInDatabase = await prismaClient.user.count({
+        where: {
+            username: user.username,
+        }
+    });
+
+    if (totalUserInDatabase !== 1) {
+        throw new ResponseError(404, 'user is not found');
+    }
+
+    const data = {}
+    if (user.name) {
+        data.name = user.name;
+    }
+    if (user.password) {
+        data.password = await bcrypt.hash(user.password, 10);
+    }
+
+    return prismaClient.user.update({
+        where: {
+            username: user.username,
+        },
+        data: data,
+        select: {
+            username: true,
+            name: true
+        }
+    });
+}
+
 export default {
-    register
+    register,
+    login,
+    get,
+    update
 };
---
+++ test/user.test.js
@@ -0,0 +1,230 @@
+import supertest from "supertest";
+import {web} from "../src/application/web.js";
+import {logger} from "../src/application/logging.js";
+import {createTestUser, getTestUser, removeTestUser} from "./test-util.js";
+import bcrypt from "bcrypt";
+
+describe('POST /api/users', () => {
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    test('should can register new user', async () => {
+        const result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "test",
+                password: "secretkey",
+                name: "test"
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("test");
+        expect(result.body.data.password).toBeUndefined();
+    });
+
+    test('should reject if request is invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "",
+                password: "",
+                name: ""
+            });
+
+        logger.info(result.body)
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    test('should reject if username already to registered', async () => {
+        let result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "test",
+                password: "secretkey",
+                name: "test"
+            });
+
+        logger.info(result.body)
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("test");
+        expect(result.body.data.password).toBeUndefined();
+
+        result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "test",
+                password: "secretkey",
+                name: "test"
+            });
+
+        logger.info(result.body)
+
+        expect(result.status).toBe(404);
+        expect(result.body.errors).toBeDefined();
+    });
+});
+
+describe('POST /api/users/login', () => {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can login', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "secretkey"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.token).toBeDefined();
+        expect(result.body.data.token).not.toBe("test");
+    });
+
+    it('should reject if request is invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "",
+                password: ""
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should reject login if password is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "salah"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should reject login if username is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "salah",
+                password: "secretkey"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+});
+
+describe('GET /api/users/current', () => {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can get user', async () => {
+        const result = await supertest(web)
+            .get('/api/users/current')
+            .set('Authorization', 'test');
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("test");
+    });
+
+    it('should reject if token is invalid', async () => {
+        const result = await supertest(web)
+            .get('/api/users/current')
+            .set('Authorization', 'wrong');
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+});
+
+describe('PATCH /api/users/current', () => {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can update user and password', async () => {
+        const result = await supertest(web)
+            .patch('/api/users/current')
+            .set('Authorization', 'test')
+            .send({
+                name: 'Dani',
+                password: 'keysecret'
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("Dani");
+        const user = await getTestUser();
+        expect(await bcrypt.compare('keysecret', user.password)).toBe(true);
+    });
+
+    it('should can update user', async () => {
+        const result = await supertest(web)
+            .patch('/api/users/current')
+            .set('Authorization', 'test')
+            .send({
+                name: 'Dani',
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("Dani");
+    });
+
+    it('should can update password', async () => {
+        const result = await supertest(web)
+            .patch('/api/users/current')
+            .set('Authorization', 'test')
+            .send({
+                password: 'keysecret'
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        const user = await getTestUser();
+        expect(await bcrypt.compare('keysecret', user.password)).toBe(true);
+    });
+
+    it('should can update password', async () => {
+        const result = await supertest(web)
+            .patch('/api/users/current')
+            .set('Authorization', 'wrong')
+            .send({});
+
+        expect(result.status).toBe(401);
+    });
+});
